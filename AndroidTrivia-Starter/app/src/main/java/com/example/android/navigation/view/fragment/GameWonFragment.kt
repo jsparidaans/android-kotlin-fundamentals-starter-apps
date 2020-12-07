71b21ceafdad9902490af3ef9703b6d807ccba9c
Joey Sparidaans <joey.sparidaans@myidbv.com>
2020-12-07 10:43:42 +0100

    updated kotlin and navigation version and moved packages

--- AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/view/fragment/GameWonFragment.kt
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/view/fragment/GameWonFragment.kt
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.example.android.navigation
+package com.example.android.navigation.view.fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -22,12 +22,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.example.android.navigation.R
 import com.example.android.navigation.databinding.FragmentGameWonBinding
 
 
 class GameWonFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+                              savedInstanceState: Bundle?): View {
         // Inflate the layout for this fragment
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_won, container, false)
--- AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/view/fragment/RulesFragment.kt
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/view/fragment/RulesFragment.kt
@@ -14,18 +14,19 @@
  * limitations under the License.
  */
 
-package com.example.android.navigation
+package com.example.android.navigation.view.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.android.navigation.R
 
-class AboutFragment : Fragment() {
+class RulesFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        return inflater.inflate(R.layout.fragment_rules, container, false)
     }
 }
