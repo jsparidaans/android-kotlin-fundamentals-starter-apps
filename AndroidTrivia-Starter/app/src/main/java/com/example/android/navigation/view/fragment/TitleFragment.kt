17e6772c315475020c40e9a71cc04302398bca99
Joey Sparidaans <joey.sparidaans@myidbv.com>
2020-12-07 12:04:38 +0100

    added drawerlayout

--- AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/view/fragment/TitleFragment.kt
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/view/fragment/TitleFragment.kt
@@ -9,11 +9,7 @@ import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.R
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
-/**
- * A simple [Fragment] subclass.
- * Use the [TitleFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class TitleFragment : Fragment() {
 
 
